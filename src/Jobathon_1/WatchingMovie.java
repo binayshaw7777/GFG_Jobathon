94117aa2459ff3aff685853a47321d54824858ef
Binay Shaw <binayshaw7777@gmail.com>
2022-09-25 22:43:05 +0530

    Watching Movie

--- src/Jobathon_1/WatchingMovie.java
+++ src/Jobathon_1/WatchingMovie.java
@@ -1,8 +1,5 @@
 package Jobathon_1;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 //https://practice.geeksforgeeks.org/contest/job-a-thon-hiring-challenge-1/problems/#
 public class WatchingMovie {
     public static void main(String[] args) {
@@ -12,43 +9,33 @@ public class WatchingMovie {
         int[] testCase2 = {9, 7, 7, 7, 4};
         int K3 = 2;
         int[] testCase3 = {4, 2, 4, 6, 7, 1, 8, 6};
+        int K4 = 3;
+        int[] testCase4 = {4, 5, 6, 7, 8};
 
-        System.out.println("-> "+solve(testCase1.length, K1, testCase1));
-        System.out.println("-> "+solve(testCase2.length, K2, testCase2));
-        System.out.println("-> "+solve(testCase3.length, K3, testCase3));
-
+        System.out.println("-> " + solve(testCase1.length, K1, testCase1));
+        System.out.println("-> " + solve(testCase2.length, K2, testCase2));
+        System.out.println("-> " + solve(testCase3.length, K3, testCase3));
+        System.out.println("-> " + solve(testCase4.length, K4, testCase4));
     }
 
-    static int solve(int N, int K, int h[]) {
-        if (h == null || N == 0 || N < K) return 0;
+    static int solve(int N, int K, int[] h) {
         int total = 0;
-        int req = N - K;
-        int[] act = new int[req];
-        Arrays.fill(act, 1);
-        int[] pow = new int[req];
 
-        if (K == 1) {
-            for (int i = 1; i < N; i++) {
-                if (h[i] > h[i-1])
-                    total++;
-            }
-            return N - total;
-        }
+        if (K == 0 || h.length == 1) return N - total;
 
         for (int i = 0; i < N; i++) {
-            if (i+K < N) {
-                pow[i] = (int) Math.pow(h[i], K);
-                for (int j = i; j < i + K; j++) {
-                    System.out.println(act[i] +" * " + h[i]);
-                    act[i] *= h[j];
-                    System.out.println("act[i] = " + act[i]);
-                }
-                System.out.println(pow[i] +" - " + act[i]);
-                if (pow[i] <= act[i])
+            int ik = i + K;
+            int j = i + 1;
+
+            while (j < N && j <= ik) {
+                if (h[j] >= h[i]) {
                     total += 1;
+                    break;
+                }
+                j++;
             }
         }
 
-        return N - total - 1;
+        return N - total;
     }
 }
